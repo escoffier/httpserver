c8a180e54d60e63cc85b7f0366525bb668d95d1e
unknown <qunfengqiu@gmail.com>
2014-08-09 15:19:31 +0800

    add multithread client

---
+++ client/threadclient/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+void* worker(void *arg)
+{
+	 int fd;
+    std::cout<<"start thread: "<<(int)arg<<std::endl;	
+	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(fd < 0)
+	{
+	    std::cout<<"socket error: "<<errno<<std::endl;
+		return 0;
+	}
+	
+    struct sockaddr_in in;
+	memset(&in, 0, sizeof(struct sockaddr_in));
+	
+	in.sin_family = AF_INET;
+	in.sin_port = htons(8081);
+	short addr = 0;
+	if(1 != inet_pton(AF_INET, "192.168.60.109", &in.sin_addr))
+	{
+	    std::cout<<"inet_pton error: "<<errno<<std::endl;
+	    return 0;
+	}
+	
+	if( -1 == connect(fd, (struct sockaddr*)&in, sizeof(struct sockaddr_in)) )
+	{
+	    std::cout<<"connect error: "<<errno<<std::endl;
+		return 0;
+	}
+
+	//char buf[] = "hello world";
+    //write(fd, buf,sizeof(buf) );
+	return 0;
+}
+const int maxthreads = 10;
+int main(int argc, char **argv )
+{
+    std::cout<<"run main"<<std::endl;
+    pthread_t threads[maxthreads];
+	pthread_attr_t attr;
+    pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE );
+
+	for(int i = 0; i < maxthreads; ++i)
+	{ 
+		std::cout<<"create thread "<<i<<std::endl;
+		pthread_create(&threads[i], &attr, worker,(void *)i );
+	}	
+	
+	for(int i = 0; i < maxthreads; ++i)
+	{
+		pthread_join(threads[i], NULL);
+	}
+   	/*
+	fd_set rset;
+	int nfd;
+	int res;
+	char buf[2048] = {0};
+	while(1)
+	{
+	    FD_SET(fileno(stdin), &rset);
+	    FD_SET(fd, &rset);
+	
+	    nfd = MAX(fileno(stdin), fd);
+		
+		res = select(nfd, &rset, 0, 0, 0);
+		
+		if(res > 0)
+		{
+		    if(FD_ISSET(fd, &rset))
+			{
+			    res = read(fd, buf, 2048);
+				if(res == 0)
+				{
+				    break;
+				}
+				
+				write(fileno(stdout), buf, res);
+			}
+			
+			if(FD_ISSET(fileno(stdin), &rset))
+			{
+				res = read(fileno(stdin), buf, 2048);
+				if(res == 0)
+				{
+				    break;
+				}
+				
+				write(fd, buf, res);
+			}
+		}
+	} 
+	*/
+
+	std::cout<<"start http server"<<std::endl;
+    //HttpServer server(8081);
+    return 0;
+}
